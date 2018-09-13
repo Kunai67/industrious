e9802fb46c04455c33c40a8e71974c6b60cf8cd5
Ian Agustin <inbagustin@gmail.com>
2018-09-13 19:17:27 +0800

    Added odo video background

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
 const {SidebarElement, SidebarService} = require('sidebarjs');
+
+import OdoBackgroundVideo from '@odopod/odo-background-video';
+
+const videoBackground = new OdoBackgroundVideo(document.querySelector('.l-video-banner'));
